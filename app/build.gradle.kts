82593cf6ab6ebd1f8333a8a1dfb07cacfc0b6082
adhinbabu <Ababu0877@conestogac.on.ca>
2023-10-30 19:22:39 -0400

    Added Registration which adds user to firebase. Forgot password is done.Login has some error with authenticaton.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures{
+        viewBinding=true;
+    }
 }
 
 dependencies {
@@ -38,7 +41,10 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:21.1.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/example/solo2squad/MainActivity.java
+++ app/src/main/java/com/example/solo2squad/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.solo2squad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.solo2squad.databinding.ActivityIntroBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ActivityIntroBinding introBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        introBinding=ActivityIntroBinding.inflate(getLayoutInflater());
+        View view = introBinding.getRoot();
+        setContentView(view);
+        introBinding.getStartedbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
