be45615f81540cbfe896bd5437ee06682e4f5981
adhinbabu <Ababu0877@conestogac.on.ca>
2023-11-08 23:42:44 -0500

    profile section 2nd page added

--- app/src/main/java/com/example/solo2squad/ProfileSection/ProfileSection2Activity.java
+++ app/src/main/java/com/example/solo2squad/ProfileSection/ProfileSection2Activity.java
@@ -1,4 +1,22 @@
 package com.example.solo2squad.ProfileSection;
 
-public class ProfileSection2Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.solo2squad.R;
+
+public class ProfileSection2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profilesection2);
+
+
+
+    }
 }
+
